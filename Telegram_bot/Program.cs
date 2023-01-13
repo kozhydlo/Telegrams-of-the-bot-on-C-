87d869bc56b97ff8f204fcd39e3700e32bbcfb0e
Mark Kozhydlo <99179725+kozhydlomark2010@users.noreply.github.com>
2023-01-13 03:04:10 +0200

    Add files via upload

---
+++ Telegram_bot/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace Telegram_Bot
+{
+    class Program
+    {
+        private static string Token { get; set; } = "Ваш токен тут";
+        private static TelegramBotClient client;
+
+        static void Main(string[] args)
+        {
+            client = new TelegramBotClient(Token);
+            client.StartReceiving();
+            client.OnMessage += OnMessageHandler;
+            Console.ReadLine();
+            client.StopReceiving();
+        }
+
+        private static async void OnMessageHandler(object sender, MessageEventArgs e)
+        {
+            var msg = e.Message;
+            if (msg.Text != null)
+            {
+                Console.WriteLine($"Прийшло оповішення з текстом: {msg.Text}");
+                switch (msg.Text)
+                {
+                    case "Стикер":
+                        await client.SendStickerAsync(
+                            chatId: msg.Chat.Id,
+                            sticker: "Силка на стікери",
+                            replyToMessageId: msg.MessageId,
+                            replyMarkup: GetButtons());
+                        break;
+                    case "Картинка":
+                        await client.SendPhotoAsync(
+                            chatId: msg.Chat.Id,
+                            photo: "Силка на стікери",
+                            replyMarkup: GetButtons());
+                        break;
+
+                    default:
+                        await client.SendTextMessageAsync(msg.Chat.Id, "Виберіть команду: ", replyMarkup: GetButtons());
+                        break;
+                }
+            }
+        }
+
+        private static IReplyMarkup GetButtons()
+        {
+            return new ReplyKeyboardMarkup
+            {
+                Keyboard = new List<List<KeyboardButton>>
+                {
+                    new List<KeyboardButton>{ new KeyboardButton { Text = "Стікер"}, new KeyboardButton { Text = "Картинка"} },
+                    new List<KeyboardButton>{ new KeyboardButton { Text = "123"}, new KeyboardButton { Text = "456"} }
+                }
+            };
+        }
+    }
+}
